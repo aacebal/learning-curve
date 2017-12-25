f04946c510e7dca07591d293abb56ab6b262d158
Adel Acebal <adelacebal@yahoo.com>
2017-12-24 22:26:51 -0500

    home page template added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LearningGraphComponent } from './learning-graph/learning-graph.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: LearningGraphComponent },
+  { path: 'dashboard', component: HomeComponent },
 ];
 
 @NgModule({
